bbd059865808f4d38a3d5048e9a8fd501423092a
George Gally <GeorgeGally@users.noreply.github.com>
2026-07-09 00:18:03 +0700

    feat: configure sidekiq-cron for belief synthesis, capture watcher, and backup

---
+++ flyd_v1/config/initializers/sidekiq_cron.rb
@@ -0,0 +1,11 @@
+schedule_file = Rails.root.join("config", "sidekiq.yml")
+
+if defined?(Sidekiq::Cron) && schedule_file.exist?
+  schedule_data = YAML.safe_load(File.read(schedule_file), permitted_classes: [Symbol])
+  schedule = schedule_data&.dig(:scheduler, :schedule)
+
+  if schedule
+    Sidekiq::Cron::Job.load_from_hash(schedule)
+    Rails.logger.info "Loaded #{schedule.keys.length} sidekiq-cron jobs: #{schedule.keys.join(", ")}"
+  end
+end
