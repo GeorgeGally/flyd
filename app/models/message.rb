f17d0793fed68940171046f7fcd4855969ad2dfc
George Gally <GeorgeGally@users.noreply.github.com>
2026-07-12 20:11:01 +0800

    feat: mark context-superseded messages explicitly

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,4 +4,8 @@ class Message < ApplicationRecord
   validates :role, presence: true, inclusion: { in: %w[user assistant system] }
 
   scope :ordered, -> { order(created_at: :asc) }
+
+  def context_superseded?
+    ActiveModel::Type::Boolean.new.cast(metadata["context_superseded"])
+  end
 end
