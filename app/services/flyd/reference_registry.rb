f2dd8ce86ccbfd54987e34411bbaee0c563345de
George Gally <GeorgeGally@users.noreply.github.com>
2026-07-12 22:58:35 +0800

    feat: validate scene artifact and build references

--- app/services/flyd/reference_registry.rb
+++ app/services/flyd/reference_registry.rb
@@ -12,12 +12,24 @@ module Flyd
     end
 
     def call
+      register_record("scene", @state.dig(:current_work, :id))
+      register_record("artifact", @state.dig(:current_work, :resolved_artifact_id))
       register_record("intent", @state.dig(:active_intent, :id))
       register_record("conversation", @state.dig(:active_interaction, :id))
       Array(@state.dig(:active_interaction, :messages)).each { |message| register_record("message", message[:id]) }
 
       register_record("surface", @state.dig(:previous_surface, :id))
-      Array(@state.dig(:previous_surface, :items)).each { |item| register_record("surface_item", item[:id]) }
+      Array(@state.dig(:previous_surface, :items)).each do |item|
+        register_record("surface_item", item[:id])
+        register_record("scene", item[:scene_id])
+      end
+
+      Array(@state[:scenes]).each do |scene|
+        register_record("scene", scene[:id])
+        register_record("artifact", scene[:resolved_artifact_id])
+      end
+      Array(@state[:artifacts]).each { |artifact| register_record("artifact", artifact[:id]) }
+      Array(@state[:builds]).each { |build| register_record("build", build[:id]) }
 
       Array(@state[:projects]).each do |project|
         register_record("project", project[:id])
