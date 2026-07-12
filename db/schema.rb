1f1261c1bf87964a153466eacc0a7fe41b8a4b09
George Gally <GeorgeGally@users.noreply.github.com>
2026-07-12 20:18:51 +0800

    db: record belief provenance in the final schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_07_12_150000) do
+ActiveRecord::Schema[8.0].define(version: 2026_07_12_160000) do
   enable_extension "pg_catalog.plpgsql"
 
   create_table "behaviours", force: :cascade do |t|
@@ -37,6 +37,7 @@ ActiveRecord::Schema[8.0].define(version: 2026_07_12_150000) do
     t.datetime "last_used_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.jsonb "source_decision_ids", default: [], null: false
     t.index ["project_id"], name: "index_beliefs_on_project_id"
   end
 
