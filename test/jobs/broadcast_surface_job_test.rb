585eb039885c330ebfd0ba94ba2615650841184c
George Gally <GeorgeGally@users.noreply.github.com>
2026-07-12 19:37:00 +0800

    test: require semantic surface morph broadcasts

--- test/jobs/broadcast_surface_job_test.rb
+++ test/jobs/broadcast_surface_job_test.rb
@@ -1,17 +1,18 @@
 require "test_helper"
 
 class BroadcastSurfaceJobTest < ActiveJob::TestCase
-  test "broadcasts the persisted surface plane" do
+  test "morphs the persisted surface plane by semantic identity" do
     surface = Surface.fallback!
     calls = []
 
-    Turbo::StreamsChannel.stub(:broadcast_replace_to, ->(*args, **kwargs) { calls << [args, kwargs] }) do
+    Turbo::StreamsChannel.stub(:broadcast_replace_to, ->(*args, **kwargs) { calls << [ args, kwargs ] }) do
       BroadcastSurfaceJob.perform_now(surface.id)
     end
 
     assert_equal 1, calls.length
     assert_equal "flyd_surface", calls.first.first.first
     assert_equal "surface_plane", calls.first.last[:target]
+    assert_equal :morph, calls.first.last[:method]
     assert_equal surface, calls.first.last[:locals][:surface]
   end
 end
