14f7b211e4e9bd5957bc7ecff80601b438bef2ba
George Gally <GeorgeGally@users.noreply.github.com>
2026-07-12 12:20:30 +0800

    test: provide dependency-free scoped stubs

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,27 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
-require "minitest/mock"
+
+module SimpleStub
+  def stub(method_name, replacement)
+    singleton = singleton_class
+    existed = singleton.method_defined?(method_name) || singleton.private_method_defined?(method_name)
+    original = method(method_name) if respond_to?(method_name, true)
+
+    singleton.define_method(method_name) do |*args, **kwargs, &block|
+      replacement.respond_to?(:call) ? replacement.call(*args, **kwargs, &block) : replacement
+    end
+    yield
+  ensure
+    if existed && original
+      singleton.define_method(method_name, original)
+    else
+      singleton.remove_method(method_name) if singleton.method_defined?(method_name)
+    end
+  end
+end
+
+Object.include(SimpleStub)
 
 module ActiveSupport
   class TestCase
