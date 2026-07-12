2a6666f18017a8cb952c39e432a4161861c8e8af
George Gally <GeorgeGally@users.noreply.github.com>
2026-07-12 19:27:15 +0800

    fix: remove Surface class-module namespace conflict

--- app/services/surface/planner.rb
+++ app/services/surface/planner.rb
@@ -1,7 +1,5 @@
-module Surface
-  class Planner
-    def self.call(active_conversation: nil)
-      Flyd::Intelligence.compose_surface(active_conversation: active_conversation)
-    end
+class Surface::Planner
+  def self.call(active_conversation: nil)
+    Flyd::Intelligence.compose_surface(active_conversation: active_conversation)
   end
 end
